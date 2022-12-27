034bff1774067871afc3106972bfbcc83a93f353
v22lel <v22julian@gmail.com>
2022-12-27 20:38:25 +0100

    isLetterOrDigit

--- src/main/java/dev/mv/utils/Utils.java
+++ src/main/java/dev/mv/utils/Utils.java
@@ -161,4 +161,10 @@ public class Utils {
     public static <T> T[] array(Collection<T> collection) {
         return (T[]) collection.toArray();
     }
+
+    public static boolean isCharLetterOrDigit(char c) {
+        return (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+            (c >= '0' && c <= '9');
+    }
 }
